6c5e0d65e3b32e00985ef6d222c844fdb58d8d80
Travis Miller <tmiller@broco.com>
2016-12-08 14:02:18 -0600

    Create 7.x branch

--- src/Flysystem/Sftp.php
+++ src/Flysystem/Sftp.php
@@ -7,37 +7,14 @@
 
 namespace Drupal\flysystem_sftp\Flysystem;
 
-use Drupal\Core\Logger\RfcLogLevel;
 use Drupal\flysystem\Flysystem\Adapter\MissingAdapter;
-use Drupal\flysystem\Plugin\FlysystemPluginInterface;
-use Drupal\flysystem\Plugin\FlysystemUrlTrait;
+use Drupal\flysystem\Flysystem\Ftp as FtpFlysystemPluginBase;
 use League\Flysystem\Sftp\SftpAdapter;
 
 /**
  * Drupal plugin for the "SFTP" Flysystem adapter.
- *
- * @Adapter(id = "sftp")
  */
-class Sftp implements FlysystemPluginInterface {
-
-  use FlysystemUrlTrait;
-
-  /**
-   * Plugin configuration.
-   *
-   * @var array
-   */
-  protected $configuration;
-
-  /**
-   * Constructs an Sftp object.
-   *
-   * @param array $configuration
-   *   Plugin configuration array.
-   */
-  public function __construct(array $configuration) {
-    $this->configuration = $configuration;
-  }
+class Sftp extends FtpFlysystemPluginBase {
 
   /**
    * {@inheritdoc}
@@ -59,35 +36,18 @@ class Sftp implements FlysystemPluginInterface {
    * {@inheritdoc}
    */
   public function ensure($force = FALSE) {
-    try {
-      $adapter = new SftpAdapter($this->configuration);
-      $adapter->connect();
+    if ($this->getAdapter() instanceof SftpAdapter) {
+      return array();
     }
 
-    catch (\LogicException $e) {
-      return [[
-        'severity' => RfcLogLevel::ERROR,
-        'message' => 'Unable to login to the SFTP server %host:%port with the provided credentials.',
-        'context' => [
-          '%host' => $this->configuration['host'],
-          '%port' => isset($this->configuration['port']) ? $this->configuration['port'] : 22,
-        ],
-      ]];
-    }
-
-    catch (\RuntimeException $e) {
-      return [[
-        'severity' => RfcLogLevel::ERROR,
-        'message' => 'The root %root is invalid or does not exist on the SFTP server %host:%port.',
-        'context' => [
-          '%root' => $this->configuration['root'],
-          '%host' => $this->configuration['host'],
-          '%port' => isset($this->configuration['port']) ? $this->configuration['port'] : 22,
-        ],
-      ]];
-    }
-
-    return [];
+    return array(array(
+      'severity' => WATCHDOG_ERROR,
+      'message' => 'There was an error connecting to the SFTP server %host:%port.',
+      'context' => array(
+        '%host' => $this->configuration['host'],
+        '%port' => isset($this->configuration['port']) ? $this->configuration['port'] : 22,
+      ),
+    ));
   }
 
 }
